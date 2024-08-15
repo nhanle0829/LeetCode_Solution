12b1b7ae74310366c953d33165c6c042a2e04163
Nhan Le <nhanle0829.work@gmail.com>
2024-08-14 23:10:42 -0700

    add basic test

--- Python/ArrayHashing/217_ContainsDuplicate.py
+++ Python/ArrayHashing/217_ContainsDuplicate.py
@@ -1,3 +1,6 @@
+from operator import truediv
+
+
 class Solution(object):
     def containsDuplicate(self, nums):
         pass
@@ -5,4 +8,7 @@ class Solution(object):
 
 if __name__ == "__main__":
     testObj = Solution()
-
+    assert testObj.containsDuplicate([1, 2, 3, 1]) is True
+    assert testObj.containsDuplicate([1, 2]) is False
+    assert testObj.containsDuplicate([1, 2, 3, 4]) is False
+    assert testObj.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) is True
