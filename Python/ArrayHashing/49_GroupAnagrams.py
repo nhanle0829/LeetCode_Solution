46906a3556e46008a6c3e3d35a90ca8eee0f1066
Nhan Le <nhanle0829.work@gmail.com>
2024-08-18 19:02:32 -0700

    add solution for leetcode #49

--- Python/ArrayHashing/49_GroupAnagrams.py
+++ Python/ArrayHashing/49_GroupAnagrams.py
@@ -1,9 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs):
-        pass
+        hash_map = {}
+        for str in strs:
+            count = [0] * 26
+            for c in str:
+                count[ord(c) - ord('a')] += 1
+            if tuple(count) not in hash_map:
+                hash_map[tuple(count)] = []
+            hash_map[tuple(count)].append(str)
+        return list(hash_map.values())
 
 if __name__ == "__main__":
     test_obj = Solution()
-    assert test_obj.groupAnagrams(["eat","tea","tan","ate","nat","bat"]) == [["tan","nat"],["bat"],["eat","tea","ate"]]
+    print(test_obj.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
     assert test_obj.groupAnagrams([""]) == [[""]]
     assert test_obj.groupAnagrams(["a"]) == [["a"]]
