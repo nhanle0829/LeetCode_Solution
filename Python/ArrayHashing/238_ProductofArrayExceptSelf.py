3137f486369eed1132261ad991b1d8a467430728
Nhan Le <nhanle0829.work@gmail.com>
2024-08-19 09:32:30 -0700

    improve space by not using prefix and postfix temporary array

--- Python/ArrayHashing/238_ProductofArrayExceptSelf.py
+++ Python/ArrayHashing/238_ProductofArrayExceptSelf.py
@@ -1,19 +1,11 @@
 class Solution:
     def productExceptSelf(self, nums):
         res = [1] * len(nums)
-        prefix = [1] * len(nums)
-        postfix = [1] * len(nums)
 
-        for i in range(len(nums)):
-            if i == 0:
-                continue
-            prefix[i] = prefix[i - 1] * nums[i - 1]
-
+        for i in range(1, len(nums)):
+            res[i] = res[i - 1] * nums[i - 1]
+        postfix = 1
         for i in range(len(nums) - 1, -1, -1):
-            if i == len(nums) - 1:
-                continue
-            postfix[i] = postfix[i + 1] * nums[i + 1]
-
-        for i in range(len(nums)):
-            res[i] = prefix[i] * postfix[i]
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
