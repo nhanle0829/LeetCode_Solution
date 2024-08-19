5f421c092a9eaca461930047ebff29ffd976af41
Nhan Le <nhanle0829.work@gmail.com>
2024-08-18 23:11:00 -0700

    add solution #238

--- Python/ArrayHashing/238_ProductofArrayExceptSelf.py
+++ Python/ArrayHashing/238_ProductofArrayExceptSelf.py
@@ -1,4 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums):
-        pass
+        res = [1] * len(nums)
+        prefix = [1] * len(nums)
+        postfix = [1] * len(nums)
 
+        for i in range(len(nums)):
+            if i == 0:
+                continue
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i == len(nums) - 1:
+                continue
+            postfix[i] = postfix[i + 1] * nums[i + 1]
+
+        for i in range(len(nums)):
+            res[i] = prefix[i] * postfix[i]
+        return res
