258ff774285bd13679d66f421859de4011790b9a
Nhan Le <nhanle0829.work@gmail.com>
2024-08-18 22:32:29 -0700

    add solution #271

--- Python/ArrayHashing/271_EncodeDecodeString.py
+++ Python/ArrayHashing/271_EncodeDecodeString.py
@@ -1,5 +1,20 @@
 class Solution:
     def encode(self, strs):
-        pass
+        res = ""
+        for s in strs:
+            res += str(len(s)) + '#' + s
+        print(res)
+        return res
+
     def decode(self, s):
-        pass
+        res = []
+        index = 0
+        while index < len(s):
+            length = ''
+            while s[index] != '#':
+                length += s[index]
+                index += 1
+            length = int(length)
+            res.append(s[index + 1: index + 1 + length])
+            index += length + 1
+        return res
