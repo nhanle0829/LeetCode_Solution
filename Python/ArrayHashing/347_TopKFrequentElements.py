2ed19c72a1e1df71e501d10f7c0e25fd77bd8a44
Nhan Le <nhanle0829.work@gmail.com>
2024-08-18 20:03:43 -0700

    add solution leetcode #347 2.0

--- Python/ArrayHashing/347_TopKFrequentElements.py
+++ Python/ArrayHashing/347_TopKFrequentElements.py
@@ -1,7 +1,16 @@
 class Solution:
     def topKFrequent(self, nums, k):
         count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        res = []
+
         for num in nums:
             count[num] = count.get(num, 0) + 1
-        count = dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
-        return list(count.keys())[:k]
+        for n, f in count.items():
+            freq[f].append(n)
+
+        for i in range(len(nums), 0, -1):
+            for j in freq[i]:
+                res.append(j)
+                if len(res) == k:
+                    return res
