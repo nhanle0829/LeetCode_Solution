acd92f5281fc34578682559a34d2dd2378b2a237
Nhan Le <nhanle0829.work@gmail.com>
2024-08-19 10:02:34 -0700

    add solution #36

--- Python/ArrayHashing/36_ValidSudoku.py
+++ Python/ArrayHashing/36_ValidSudoku.py
@@ -1,3 +1,23 @@
+from collections import defaultdict
+
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        pass
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squa = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+
+                if (board[i][j] in rows[i] or
+                        board[i][j] in cols[j] or
+                        board[i][j] in squa[(i // 3, j // 3)]):
+                    return False
+
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                squa[(i // 3, j // 3)].add(board[i][j])
+
+        return True
