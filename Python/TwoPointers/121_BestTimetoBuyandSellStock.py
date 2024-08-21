edb24acced533a17816b671adad36d21bd14d9f0
Nhan Le <nhanle0829.work@gmail.com>
2024-08-21 14:18:09 -0700

    add solution for #121

--- Python/TwoPointers/121_BestTimetoBuyandSellStock.py
+++ Python/TwoPointers/121_BestTimetoBuyandSellStock.py
@@ -1,3 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        profit = 0
+        lowest = float("inf")
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            else:
+                profit = max(profit, price - lowest)
+
+        return profit
