47a8e884d43708c3394f493ad2774f92bccb2cb2
Nhan Le <nhanle0829.work@gmail.com>
2024-08-22 11:51:36 -0700

    add solution for #424

--- Python/SlidingWindow/424_LongestRepeatingCharacterReplacement.py
+++ Python/SlidingWindow/424_LongestRepeatingCharacterReplacement.py
@@ -1,2 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        longest = 0
+        left = 0
+        for right in range(len(s)):
+            count[s[right]] = count.get(s[right], 0) + 1
+            while (right - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+            longest = max(longest, right - left + 1)
+        return longest
