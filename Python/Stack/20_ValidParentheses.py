06fd776e4596ae2f766239b4f0d8364e11e0578a
Nhan Le <nhanle0829.work@gmail.com>
2024-08-23 12:06:05 -0700

    add solution for #20

--- Python/Stack/20_ValidParentheses.py
+++ Python/Stack/20_ValidParentheses.py
@@ -1,2 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        match_bracket = {'(': ')', '[': ']', '{': '}'}
+        stack = []
+        for bracket in s:
+            if bracket in match_bracket:
+                stack.append(bracket)
+            else:
+                if not stack or match_bracket[stack.pop()] != bracket:
+                    return False
+        return not stack
