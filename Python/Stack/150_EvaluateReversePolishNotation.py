1ed4a08c4135461b56d5ae6ad79985d8e7b14769
Nhan Le <nhanle0829.work@gmail.com>
2024-08-24 11:48:13 -0700

    add solution for #150

--- Python/Stack/150_EvaluateReversePolishNotation.py
+++ Python/Stack/150_EvaluateReversePolishNotation.py
@@ -1,2 +1,20 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        operator = ['+', '-', '*', '/']
+        stack = []
+        for token in tokens:
+            if token in operator:
+                operand_2 = stack.pop()
+                operand_1 = stack.pop()
+                if token == '+':
+                    res = operand_1 + operand_2
+                elif token == '-':
+                    res = operand_1 - operand_2
+                elif token == '*':
+                    res = operand_1 * operand_2
+                else:
+                    res = int(operand_1 / operand_2)
+                stack.append(res)
+            else:
+                stack.append(int(token))
+        return stack[0]
