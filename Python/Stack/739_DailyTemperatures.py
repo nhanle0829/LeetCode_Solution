5cb05ed37bca645aefd51eea7563f04bd5f0ed55
Nhan Le <nhanle0829.work@gmail.com>
2024-08-24 12:34:01 -0700

    add solution for #739

--- Python/Stack/739_DailyTemperatures.py
+++ Python/Stack/739_DailyTemperatures.py
@@ -1,3 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
+        result = [0] * len(temperatures)
+        stack = []  # (temp, index)
+        for index, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                pre_temp, pre_index = stack.pop()
+                result[pre_index] = index - pre_index
+            stack.append((temp, index))
+        return result
