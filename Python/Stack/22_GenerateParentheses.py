35e74602adbe3a950b3d8b50fe08d62f9f36ec75
Nhan Le <nhanle0829.work@gmail.com>
2024-08-24 12:15:14 -0700

    add solution for #22

--- Python/Stack/22_GenerateParentheses.py
+++ Python/Stack/22_GenerateParentheses.py
@@ -1,3 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
+        result = []
+        stack = ['(']
+
+        def backtrack(openN, closeN):
+            if openN == n and closeN == n:
+                result.append(''.join(stack))
+                return
+            if openN < n:
+                stack.append('(')
+                backtrack(openN + 1, closeN)
+                stack.pop()
+            if closeN < openN:
+                stack.append(')')
+                backtrack(openN, closeN + 1)
+                stack.pop()
+
+        backtrack(1, 0)
+        return result
