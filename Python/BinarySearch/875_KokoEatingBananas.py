19c7672a5a45d035838aa8f4daefdcdbe0f1f218
Nhan Le <nhanle0829.work@gmail.com>
2024-08-26 14:47:53 -0700

    add solution for #875

--- Python/BinarySearch/875_KokoEatingBananas.py
+++ Python/BinarySearch/875_KokoEatingBananas.py
@@ -1,3 +1,16 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
+        left_rate, right_rate = 1, max(piles)
+        min_eat_rate = right_rate
+        while left_rate <= right_rate:
+            mid_rate = left_rate + (right_rate - left_rate) // 2
+            hour_need = 0
+            for pile in piles:
+                hour_need += math.ceil(pile / mid_rate)
+            if hour_need > h:
+                left_rate = mid_rate + 1
+            else:
+                min_eat_rate = mid_rate
+                right_rate = mid_rate - 1
+
+        return min_eat_rate
