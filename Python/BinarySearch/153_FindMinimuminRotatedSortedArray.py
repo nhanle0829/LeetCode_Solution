b36c60c7800ae6e9ba7ac4f8b2254dce794af26d
Nhan Le <nhanle0829.work@gmail.com>
2024-08-26 15:57:20 -0700

    add solution for #153

--- Python/BinarySearch/153_FindMinimuminRotatedSortedArray.py
+++ Python/BinarySearch/153_FindMinimuminRotatedSortedArray.py
@@ -1,3 +1,15 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        
+        left, right = 0, len(nums) - 1
+        min_value = nums[0]
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= nums[left]:
+                min_value = min(min_value, nums[left])
+                left = mid + 1
+            else:
+                right = mid - 1
+                min_value = min(min_value, nums[mid])
+
+        return min_value
+    
