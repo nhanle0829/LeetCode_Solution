db248180961924f7e7441ddf6cf2ab987e68ca0d
Nhan Le <nhanle0829.work@gmail.com>
2024-08-28 16:23:19 -0700

    add solution for #143

--- Python/LinkedList/143_ReorderList.py
+++ Python/LinkedList/143_ReorderList.py
@@ -3,4 +3,24 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-    
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev_second = slow.next = None
+        while second:
+            temp = second.next
+            second.next = prev_second
+            prev_second = second
+            second = temp
+
+        first = head
+        second = prev_second
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
