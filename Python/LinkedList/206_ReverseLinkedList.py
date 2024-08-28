9eedae36ecc784780f5aee52ae9cbad5cba2c596
Nhan Le <nhanle0829.work@gmail.com>
2024-08-28 15:10:14 -0700

    add solution for #206

--- Python/LinkedList/206_ReverseLinkedList.py
+++ Python/LinkedList/206_ReverseLinkedList.py
@@ -1,3 +1,9 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        prev, current = None, head
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+        return prev
