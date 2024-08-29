b7ea9a436e11accbc691db9fc54fe03b6c3c90b5
Nhan Le <nhanle0829.work@gmail.com>
2024-08-29 14:18:13 -0700

    add solution for #138

--- Python/LinkedList/138_CopyListwithRandomPointer.py
+++ Python/LinkedList/138_CopyListwithRandomPointer.py
@@ -1,3 +1,16 @@
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        
+        node_to_copy = {None: None}
+        node_curr = head
+        while node_curr:
+            node_to_copy[node_curr] = ListNode(node_curr.val)
+            node_curr = node_curr.next
+
+        node_curr = head
+        while node_curr:
+            copy = node_to_copy[node_curr]
+            copy.next = node_to_copy[node_curr.next]
+            copy.random = node_to_copy[node_curr.random]
+            node_curr = node_curr.next
+
+        return node_to_copy[head]
