22a65b40a1e1fa50114361faec4c3819fa63cbd9
Nhan Le <nhanle0829.work@gmail.com>
2024-08-30 23:04:41 -0700

    add solution for #146

--- Python/LinkedList/146_LRUCache.py
+++ Python/LinkedList/146_LRUCache.py
@@ -1,7 +1,41 @@
-class LRUCache:
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
+
+class LRUCache:
     def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.left, self.right = ListNode(0, 0), ListNode(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
 
     def get(self, key: int) -> int:
+        if key in self.cache:
+            self.delete(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.delete(self.cache[key])
+        self.cache[key] = ListNode(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.delete(lru)
+            del self.cache[lru.key]
+
+    def insert(self, node):
+        left_node, right_node = self.right.prev, self.right
+        node.next, node.prev = right_node, left_node
+        left_node.next, right_node.prev = node, node
+
+    def delete(self, node):
+        left_node, right_node = node.prev, node.next
+        left_node.next, right_node.prev = right_node, left_node
