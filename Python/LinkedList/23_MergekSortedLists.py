68a11216b1af00827dae8352ed8205a1ff61b04d
Nhan Le <nhanle0829.work@gmail.com>
2024-08-31 21:04:35 -0700

    add solution for #23

--- Python/LinkedList/23_MergekSortedLists.py
+++ Python/LinkedList/23_MergekSortedLists.py
@@ -1,3 +1,28 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
+        if not lists:
+            return
+
+        while len(lists) > 1:
+            merged_list = []
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i + 1] if (i + 1) < len(lists) else None
+                merged_list.append(self.merge_two_list(list1, list2))
+            lists = merged_list
+
+        return lists[0]
+
+        def merge_two_list(self, list1, list2):
+            dummy = ListNode()
+            curr = dummy
+            while list1 and list2:
+                if list1.val <= list2.val:
+                    curr.next = list1
+                    list1 = list1.next
+                else:
+                    curr.next = list2
+                    list2 = list2.next
+                curr = curr.next
+            curr.next = list1 if list1 else list2
+            return dummy.next
