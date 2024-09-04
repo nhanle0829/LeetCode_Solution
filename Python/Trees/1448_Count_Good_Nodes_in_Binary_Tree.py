3410f4b80a7df051a19814af367b14a4ce397b48
Nhan Le <nhanle0829.work@gmail.com>
2024-09-03 23:08:43 -0700

    add solution for #1448

--- Python/Trees/1448_Count_Good_Nodes_in_Binary_Tree.py
+++ Python/Trees/1448_Count_Good_Nodes_in_Binary_Tree.py
@@ -1,3 +1,14 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        
+        def dfs(node, max_val):
+            if node is None:
+                return 0
+            res = 0
+            if node.val >= max_val:
+                res = 1
+                max_val = node.val
+            res += dfs(node.left, max_val)
+            res += dfs(node.right, max_val)
+            return res
+
+        return dfs(root, root.val)
