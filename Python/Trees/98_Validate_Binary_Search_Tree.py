7c4aa38d339355b885f6d9e9d59befbdcfdf8dc3
Nhan Le <nhanle0829.work@gmail.com>
2024-09-04 19:06:11 -0700

    add solution for #98

--- Python/Trees/98_Validate_Binary_Search_Tree.py
+++ Python/Trees/98_Validate_Binary_Search_Tree.py
@@ -1,3 +1,11 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
+        def valid(node, left, right):
+            if node is None:
+                return True
+            if not (left < node.val and node.val < right):
+                return False
+
+            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+
+        return valid(root, float("-inf"), float("inf"))
