b9c32e069aac7d6929ad4c5d6d4422c24c791255
Nhan Le <nhanle0829.work@gmail.com>
2024-09-04 19:22:35 -0700

    add solution for #230

--- Python/Trees/230_Kth_Smallest_Element_in_a_BST.py
+++ Python/Trees/230_Kth_Smallest_Element_in_a_BST.py
@@ -1,3 +1,13 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
