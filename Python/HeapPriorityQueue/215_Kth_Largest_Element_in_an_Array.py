8617f6b35b89a6f0de2c9ad6b80bae3511fe2c64
Nhan Le <nhanle0829.work@gmail.com>
2024-09-06 15:42:35 -0700

    add solution for #215

--- Python/HeapPriorityQueue/215_Kth_Largest_Element_in_an_Array.py
+++ Python/HeapPriorityQueue/215_Kth_Largest_Element_in_an_Array.py
@@ -1,2 +1,8 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums = [-num for num in nums]
+        heapq.heapify(nums)
+
+        for i in range(0, k - 1):
+            heapq.heappop(nums)
+        return -nums[0]
