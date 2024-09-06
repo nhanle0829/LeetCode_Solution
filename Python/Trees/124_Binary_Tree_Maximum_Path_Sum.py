d138b76c72b9ce07e25092110adfb993b100d76c
Nhan Le <nhanle0829.work@gmail.com>
2024-09-05 18:53:54 -0700

    add solution for #124

--- Python/Trees/124_Binary_Tree_Maximum_Path_Sum.py
+++ Python/Trees/124_Binary_Tree_Maximum_Path_Sum.py
@@ -1,3 +1,20 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        
+        res = root.val
+
+        def dfs(root):
+            if root is None:
+                return 0
+
+            nonlocal res
+
+            left_max = dfs(root.left)
+            right_max = dfs(root.right)
+            left_max = max(left_max, 0)
+            right_max = max(right_max, 0)
+
+            res = max(res, root.val + left_max + right_max)
+            return root.val + max(left_max, right_max)
+
+        dfs(root)
+        return res
