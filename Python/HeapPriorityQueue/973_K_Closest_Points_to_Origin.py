8e3997c31ed075e36e4b08d07e47e505c3bcfd35
Nhan Le <nhanle0829.work@gmail.com>
2024-09-06 15:25:48 -0700

    add solution for #973

--- Python/HeapPriorityQueue/973_K_Closest_Points_to_Origin.py
+++ Python/HeapPriorityQueue/973_K_Closest_Points_to_Origin.py
@@ -1,2 +1,10 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # return sorted(points, key=lambda x: x[0] ** 2 + x[1] ** 2)[:k]
+        distances = [(point[0] ** 2 + point[1] ** 2, point[0], point[1]) for point in points]
+        heapq.heapify(distances)
+        res = []
+        for i in range(k):
+            distance, point1, point2 = heapq.heappop(distances)
+            res.append([point1, point2])
+        return res
