6a67a8356dad3b923ed1ba120cb83e93772490e8
Nhan Le <nhanle0829.work@gmail.com>
2024-09-06 18:36:11 -0700

    add solution for #3217

--- Python/LinkedList/3217_Delete_Nodes_From_Linked_List_Present_in_Array.py
+++ Python/LinkedList/3217_Delete_Nodes_From_Linked_List_Present_in_Array.py
@@ -1,3 +1,14 @@
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        nums = set(nums)
+        dummy = ListNode(0, head)
+        curr = head
+        prev = dummy
+        while curr is not None:
+            if curr.val in nums:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+
+        return dummy.next
