feca3a6334ef49d3b51930b6f9322bdfdcfb297f
Nhan Le <nhanle0829.work@gmail.com>
2024-09-07 11:59:43 -0700

    add solution for #621

--- Python/HeapPriorityQueue/621_Task_Scheduler.py
+++ Python/HeapPriorityQueue/621_Task_Scheduler.py
@@ -1,3 +1,12 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        
+        counter = collections.Counter(tasks)
+        max_freq = max(counter.values())
+
+        max_freq_tasks = 0
+        for freq in counter.values():
+            if max_freq == freq:
+                max_freq_tasks += 1
+
+        min_time = (max_freq - 1) * (n + 1) + max_freq_tasks
+        return max(min_time, len(tasks))
