144d766103a69abbe8cfd9396772ad2595097d11
Nhan Le <nhanle0829.work@gmail.com>
2024-09-06 18:11:47 -0700

    add solution for #1299

--- Python/ArrayHashing/1299_Replace_Elements_with_Greatest_Element_on_Right_Side.py
+++ Python/ArrayHashing/1299_Replace_Elements_with_Greatest_Element_on_Right_Side.py
@@ -1,3 +1,9 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        
+        right_max = -1
+        for i in range(len(arr) - 1, -1, -1):
+            cache = arr[i]
+            arr[i] = right_max
+            if cache > right_max:
+                right_max = cache
+        return arr
