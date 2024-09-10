73cbf2a10a9d097a5a4d6d01aa78fb1612d004bb
Nhan Le <nhanle0829.work@gmail.com>
2024-09-09 23:10:08 -0700

    add solution for #46

--- Python/BackTracking/46_Permutations.py
+++ Python/BackTracking/46_Permutations.py
@@ -1,3 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
+        res = []
+        permutation = []
+        included = [False] * len(nums)
+
+        def dfs(choices):
+            if choices >= len(nums):
+                res.append(permutation.copy())
+                return
+
+            for i in range(len(nums)):
+                if not included[i]:
+                    included[i] = True
+                    permutation.append(nums[i])
+                    dfs(choices + 1)
+
+                    permutation.pop()
+                    included[i] = False
+
+        dfs(0)
+        return res
