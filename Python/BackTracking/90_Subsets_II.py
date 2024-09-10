3d42fd39d14cd97ca570e5fe1305c6c19e739a6b
Nhan Le <nhanle0829.work@gmail.com>
2024-09-10 04:12:23 -0700

    add solution for #90

--- Python/BackTracking/90_Subsets_II.py
+++ Python/BackTracking/90_Subsets_II.py
@@ -1,3 +1,22 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        
+        nums.sort()
+        res = []
+        subset = []
+
+        def backtrack(index):
+            if index >= len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[index])
+            backtrack(index + 1)
+            subset.pop()
+
+            index += 1
+            while index < len(nums) and nums[index] == nums[index - 1]:
+                index += 1
+            backtrack(index)
+
+        backtrack(0)
+        return res
