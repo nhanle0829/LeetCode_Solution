94a43ff1bb233cf1c8f6bb3c33df2f647b6da149
Nhan Le <nhanle0829.work@gmail.com>
2024-09-09 22:50:05 -0700

    add solution for #39

--- Python/BackTracking/39_Combination_Sum.py
+++ Python/BackTracking/39_Combination_Sum.py
@@ -1,3 +1,21 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
+        res = []
+        path = []
+
+        def dfs(index, total):
+            if index >= len(candidates) or total > target:
+                return
+
+            if total == target:
+                res.append(path[:])
+                return
+
+            path.append(candidates[index])
+            dfs(index, total + candidates[index])
+
+            path.pop()
+            dfs(index + 1, total)
+
+        dfs(0, 0)
+        return res
