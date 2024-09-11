831302c2bb27f2214a780e680d690deef12eac60
Nhan Le <nhanle0829.work@gmail.com>
2024-09-11 01:36:29 -0700

    add solution for #205

--- Python/ArrayHashing/205_Isomorphic_Strings.py
+++ Python/ArrayHashing/205_Isomorphic_Strings.py
@@ -1,2 +1,9 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        map_ST, map_TS = {}, {}
+        for c1, c2 in zip(s, t):
+            if (c1 in map_ST and map_ST[c1] != c2) or (c2 in map_TS and map_TS[c2] != c1):
+                return False
+            map_ST[c1] = c2
+            map_TS[c2] = c1
+        return True
