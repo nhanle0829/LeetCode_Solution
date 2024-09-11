4b90e2f7d5f43f20267705f4a63127f8133144c3
Nhan Le <nhanle0829.work@gmail.com>
2024-09-11 02:11:59 -0700

    add solution for #605

--- Python/ArrayHashing/605_Can_Place_Flowers.py
+++ Python/ArrayHashing/605_Can_Place_Flowers.py
@@ -1,2 +1,12 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        empty = 0 if flowerbed[0] else 1
+        for flower in flowerbed:
+            if flower:
+                n -= int((empty - 1) / 2)
+                empty = 0
+            else:
+                empty += 1
+
+        n -= empty // 2
+        return n <= 0
