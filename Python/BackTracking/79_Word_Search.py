3d5ee8ce5f90c4fc2523f6cfa2e9d2703bd38d42
Nhan Le <nhanle0829.work@gmail.com>
2024-09-10 18:40:52 -0700

    add solution for #79

--- Python/BackTracking/79_Word_Search.py
+++ Python/BackTracking/79_Word_Search.py
@@ -1,3 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def backtrack(rows, cols, index):
+            if index == len(word):
+                return True
+            if (rows < 0 or rows >= ROWS or
+                    cols < 0 or cols >= COLS or
+                    word[index] != board[rows][cols] or
+                    (rows, cols) in path):
+                return False
+
+            path.add((rows, cols))
+            res = (backtrack(rows + 1, cols, index + 1) or
+                   backtrack(rows - 1, cols, index + 1) or
+                   backtrack(rows, cols + 1, index + 1) or
+                   backtrack(rows, cols - 1, index + 1))
+            path.remove((rows, cols))
+            return res
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if backtrack(i, j, 0):
+                    return True
+        return False
