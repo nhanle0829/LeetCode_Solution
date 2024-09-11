75f00b2acae0e925a41630c62dd047153963d59a
Nhan Le <nhanle0829.work@gmail.com>
2024-09-11 00:45:43 -0700

    add solution for #27

--- Python/ArrayHashing/27_Remove_Element.py
+++ Python/ArrayHashing/27_Remove_Element.py
@@ -1,3 +1,13 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            while left <= right and nums[left] != val:
+                left += 1
+            while left <= right and nums[right] == val:
+                right -= 1
+            if left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        return left
