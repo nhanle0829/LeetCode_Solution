90c7bf6c939153b338080ba678413ed8d70f4bcf
Nhan Le <nhanle0829.work@gmail.com>
2024-09-10 22:42:15 -0700

    add solution for #58

--- Python/ArrayHashing/58_Length_of_Last_Word.py
+++ Python/ArrayHashing/58_Length_of_Last_Word.py
@@ -1,3 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        
+        length = 0
+        for char in s[::-1]:
+            if char == ' ':
+                if length >= 1:
+                    return length
+            else:
+                length += 1
+        return length
