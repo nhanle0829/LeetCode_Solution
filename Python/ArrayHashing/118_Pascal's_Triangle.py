29558d8ed5e5f6f8e466169631cad1df1c41b6c4
Nhan Le <nhanle0829.work@gmail.com>
2024-09-10 23:02:26 -0700

    add solution for #118

--- Python/ArrayHashing/118_Pascal's_Triangle.py
+++ Python/ArrayHashing/118_Pascal's_Triangle.py
@@ -1,3 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
+        res = [[1]]
+        for rowth in range(2, numRows + 1):
+            new_row = [1]
+            for i in range(rowth - 2):
+                new_row.append(res[-1][i] + res[-1][i + 1])
+            new_row.append(1)
+            res.append(new_row)
+        return res
