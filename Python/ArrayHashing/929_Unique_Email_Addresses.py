22fab4b916fc9fd37733f2b238f821b955911d14
Nhan Le <nhanle0829.work@gmail.com>
2024-09-11 00:51:42 -0700

    add solution for #929

--- Python/ArrayHashing/929_Unique_Email_Addresses.py
+++ Python/ArrayHashing/929_Unique_Email_Addresses.py
@@ -1,3 +1,10 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        
+        unique_email = set()
+        for email in emails:
+            local_name, host_name = email.split('@')
+            local_name = local_name.split('+')[0]
+            local_name = local_name.replace('.', '')
+            email = local_name + '@' + host_name
+            unique_email.add(email)
+        return len(unique_email)
