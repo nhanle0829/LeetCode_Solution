556fb1177846188367cbc1057ceaeb8cd9bcd773
Nhan Le <nhanle0829.work@gmail.com>
2024-09-10 17:25:24 -0700

    add solution for #40

--- Python/BackTracking/40_Combination_Sum_II.py
+++ Python/BackTracking/40_Combination_Sum_II.py
@@ -1,2 +1,23 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        combine_sum = []
+
+        def backtrack(index, total):
+            if total == target:
+                res.append(combine_sum.copy())
+                return
+            if total > target or index >= len(candidates):
+                return
+
+            combine_sum.append(candidates[index])
+            backtrack(index + 1, total + candidates[index])
+            combine_sum.pop()
+
+            while index + 1 < len(candidates) and candidates[index] == candidates[index + 1]:
+                index += 1
+            backtrack(index + 1, total)
+
+        backtrack(0, 0)
+        return res
