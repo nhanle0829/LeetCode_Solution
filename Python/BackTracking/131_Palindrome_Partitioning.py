310e972f4b78f94a13549e1d1d9807f60ac8c6ba
Nhan Le <nhanle0829.work@gmail.com>
2024-09-12 15:24:36 -0700

    add solution for #131

--- Python/BackTracking/131_Palindrome_Partitioning.py
+++ Python/BackTracking/131_Palindrome_Partitioning.py
@@ -1,3 +1,26 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        
+        res = []
+        part = []
+
+        def backtrack(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+
+            for j in range(i, len(s)):
+                if self.is_palindrome(s, i, j):
+                    part.append(s[i: j + 1])
+                    backtrack(j + 1)
+                    part.pop()
+
+        backtrack(0)
+        return res
+
+    def is_palindrome(self, s, left, right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
