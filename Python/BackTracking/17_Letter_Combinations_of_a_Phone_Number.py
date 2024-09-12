0ea7ee25f79ad2517506d6a2ac800d80d1d71834
Nhan Le <nhanle0829.work@gmail.com>
2024-09-12 15:41:56 -0700

    add solution for #17

--- Python/BackTracking/17_Letter_Combinations_of_a_Phone_Number.py
+++ Python/BackTracking/17_Letter_Combinations_of_a_Phone_Number.py
@@ -1,3 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
+        num_to_char = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        res = []
+
+        def backtrack(i, curr_s):
+            if len(curr_s) == len(digits):
+                res.append(curr_s)
+                return
+
+            for char in num_to_char[digits[i]]:
+                backtrack(i + 1, curr_s + char)
+
+        if digits:
+            backtrack(0, "")
+        return res
