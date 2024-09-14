c3c0d2670a78633b2823a75770014a1a53d491a8
Nhan Le <nhanle0829.work@gmail.com>
2024-09-14 15:17:19 -0700

    add solution for #208

--- Python/Tries/208_Implement_Trie_Prefix_Tree.py
+++ Python/Tries/208_Implement_Trie_Prefix_Tree.py
@@ -1,9 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.end_of_word = False
+
 class Trie:
 
     def __init__(self):
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            index = ord(char) - ord('a')
+            if not curr.children[index]:
+                curr.children[index] = TrieNode()
+            curr = curr.children[index]
+        curr.end_of_word = True
 
     def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            index = ord(char) - ord('a')
+            if not curr.children[index]:
+                return False
+            curr = curr.children[index]
+        return curr.end_of_word
 
     def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            index = ord(char) - ord('a')
+            if not curr.children[index]:
+                return False
+            curr = curr.children[index]
+        return True
