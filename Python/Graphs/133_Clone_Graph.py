8ea4d97c9b78e4bc97e30b6d825cf950f2007d9a
Nhan Le <nhanle0829.work@gmail.com>
2024-09-13 19:03:05 -0700

    add solution for #133

--- Python/Graphs/133_Clone_Graph.py
+++ Python/Graphs/133_Clone_Graph.py
@@ -1,4 +1,16 @@
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        
+        old_to_new = {}
+
+        def dfs(node):
+            if node in old_to_new:
+                return old_to_new[node]
+
+            new_node = Node(node.val)
+            old_to_new[node] = new_node
+
+            new_node.neighbors = [dfs(nei) for nei in node.neighbors]
+            return new_node
+
+        return dfs(node) if node else None
