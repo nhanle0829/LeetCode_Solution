71a3cccb058e372f6faff3dc27265193a8aeeb2d
Nhan Le <nhanle0829.work@gmail.com>
2024-09-13 17:22:51 -0700

    add solution for #200

--- Python/Graphs/200_Number_of_Islands.py
+++ Python/Graphs/200_Number_of_Islands.py
@@ -1,3 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
+        ROWS, COLS = len(grid), len(grid[0])
+        res = 0
+
+        def dfs(row, col):
+            if (row < 0 or row >= ROWS or
+                col < 0 or col >= COLS or
+                grid[row][col] == "0"):
+                return
+
+            grid[row][col] = "0"
+
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    res += 1
+
+        return res
