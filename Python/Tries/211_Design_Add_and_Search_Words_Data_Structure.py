06c93f50c3bb374bcbabcd69ef9e3f779ae47139
Nhan Le <nhanle0829.work@gmail.com>
2024-09-14 17:09:57 -0700

    add solution for #211

--- Python/Tries/211_Design_Add_and_Search_Words_Data_Structure.py
+++ Python/Tries/211_Design_Add_and_Search_Words_Data_Structure.py
@@ -1,7 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+
 class WordDictionary:
 
     def __init__(self):
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.end_of_word = True
 
     def search(self, word: str) -> bool:
+
+        def dfs(index, root):
+            curr = root
+            for i in range(index, len(word)):
+                if word[i] == '.':
+                    for child in curr.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if word[i] not in curr.children:
+                        return False
+                    curr = curr.children[word[i]]
+            return curr.end_of_word
+
+        return dfs(0, self.root)
