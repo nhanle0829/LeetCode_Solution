221a5168b0681bef08710f43120e8314dc9196b8
Nhan Le <nhanle0829.work@gmail.com>
2024-09-16 23:54:40 -0700

    add solution for #496

--- Python/ArrayHashing/496_Next_Greater_Element_I.py
+++ Python/ArrayHashing/496_Next_Greater_Element_I.py
@@ -1,2 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hash_map = {}
+        for i, n in enumerate(nums1):
+            hash_map[n] = i
+
+        ans = [-1] * len(nums1)
+        stack = []
+        for num in nums2:
+            while stack and num > stack[-1]:
+                val = stack.pop()
+                ans[hash_map[val]] = num
+            if num in hash_map:
+                stack.append(num)
+        return ans
