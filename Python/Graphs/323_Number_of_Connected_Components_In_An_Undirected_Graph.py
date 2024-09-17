e7e740efa4cd8fd81e1b4f311f1df188b7cb5a2f
Nhan Le <nhanle0829.work@gmail.com>
2024-09-17 11:08:21 -0700

    add solution for #323

--- Python/Graphs/323_Number_of_Connected_Components_In_An_Undirected_Graph.py
+++ Python/Graphs/323_Number_of_Connected_Components_In_An_Undirected_Graph.py
@@ -1,2 +1,25 @@
 class Solution:
+    def __find(self, vertex):
+        root = self.subsets[vertex]
+        while root != self.subsets[root]:
+            root = self.subsets[root]
+
+        while self.subsets[vertex] != root:
+            nxt = self.subsets[vertex]
+            self.subsets[vertex] = root
+            vertex = nxt
+        return root
+
+    def __union(self, v1, v2):
+        root1, root2 = self.__find(v1), self.__find(v2)
+        if root1 != root2:
+            self.subsets[root1] = root2
+            self.component -= 1
+
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        self.subsets = [i for i in range(n)]
+        self.component = n
+
+        for v1, v2 in edges:
+            self.__union(v1, v2)
+        return self.component
