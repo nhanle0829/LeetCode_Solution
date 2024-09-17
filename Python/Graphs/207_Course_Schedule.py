31f2dedd778f7f2d432bb8a9a64e19ef6ddb3be8
Nhan Le <nhanle0829.work@gmail.com>
2024-09-16 23:30:43 -0700

    add solution 2.0 for #207

--- Python/Graphs/207_Course_Schedule.py
+++ Python/Graphs/207_Course_Schedule.py
@@ -17,3 +17,32 @@ class Solution:
                 if in_degree[nei] == 0:
                     queue.append(nei)
         return count == numCourses
+
+
+class Solution2:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre_map = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            pre_map[crs].append(pre)
+
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if not pre_map[crs]:
+                return True
+
+            visited.add(crs)
+            for pre in pre_map[crs]:
+                if not dfs(pre):
+                    return False
+
+            visited.remove(crs)
+            pre_map[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
