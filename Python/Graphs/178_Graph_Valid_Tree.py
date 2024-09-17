233ffcca8d5de31336fbcd373bbb6723f1cce423
Nhan Le <nhanle0829.work@gmail.com>
2024-09-17 10:59:51 -0700

    add solution 2.0 for #178

--- Python/Graphs/178_Graph_Valid_Tree.py
+++ Python/Graphs/178_Graph_Valid_Tree.py
@@ -25,3 +25,27 @@ class Solution:
             if not self.__union(v1, v2):
                 return False
         return self.component == 1
+
+
+class Solution2:
+    def validTree(self, n, edges):
+        adj_list = {i: [] for i in range(n)}
+        for v1, v2 in edges:
+            adj_list[v1].append(v2)
+            adj_list[v2].append(v1)
+
+        visited = set()
+
+        def dfs(vertex, prev):
+            if vertex in visited:
+                return False
+
+            visited.add(vertex)
+            for nei in adj_list[vertex]:
+                if nei == prev:
+                    continue
+                if not dfs(nei, vertex):
+                    return False
+            return True
+
+        return dfs(0, -1) and len(visited) == n
