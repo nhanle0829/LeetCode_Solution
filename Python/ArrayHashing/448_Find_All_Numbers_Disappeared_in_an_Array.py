151220e0a60822bc2c553476b9e414eb905f0a94
Nhan Le <nhanle0829.work@gmail.com>
2024-09-18 10:52:24 -0700

    add solution for #448

--- Python/ArrayHashing/448_Find_All_Numbers_Disappeared_in_an_Array.py
+++ Python/ArrayHashing/448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -1,3 +1,11 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        
+        for num in nums:
+            i = abs(num) - 1
+            nums[i] = -1 * abs(nums[i])
+
+        res = []
+        for i, val in enumerate(nums):
+            if val > 0:
+                res.append(i + 1)
+        return res
