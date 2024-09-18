efeb7ee18044e97001bc6a3e22e35a699be71c7e
Nhan Le <nhanle0829.work@gmail.com>
2024-09-18 10:10:39 -0700

    add solution for #303

--- Python/ArrayHashing/303_Range_Sum_Query_Immutable.py
+++ Python/ArrayHashing/303_Range_Sum_Query_Immutable.py
@@ -1,7 +1,10 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-
+        self.prefix = [0]
+        for num in nums:
+            self.prefix.append(num + self.prefix[-1])
 
     def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right + 1] - self.prefix[left]
 
