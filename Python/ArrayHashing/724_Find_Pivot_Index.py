d599d100b22b4e400d34fdec0ed0e13598627047
Nhan Le <nhanle0829.work@gmail.com>
2024-09-18 09:48:27 -0700

    add solution for #724

--- Python/ArrayHashing/724_Find_Pivot_Index.py
+++ Python/ArrayHashing/724_Find_Pivot_Index.py
@@ -1,3 +1,10 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        
+        total = sum(nums)
+        left_sum = 0
+        for pivot, val in enumerate(nums):
+            right_sum = total - left_sum - val
+            if right_sum == left_sum:
+                return pivot
+            left_sum += val
+        return -1
