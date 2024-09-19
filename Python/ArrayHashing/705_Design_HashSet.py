6d848442b50c4f27b6229dc3d35d963050466788
Nhan Le <nhanle0829.work@gmail.com>
2024-09-19 10:41:48 -0700

    add solution for #705

--- Python/ArrayHashing/705_Design_HashSet.py
+++ Python/ArrayHashing/705_Design_HashSet.py
@@ -1,10 +1,36 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
 class MyHashSet:
 
     def __init__(self):
+        self.hash_map = [ListNode(0) for i in range(10_000)]
 
     def add(self, key: int) -> None:
+        hash_val = key % 10_000
+        curr = self.hash_map[hash_val]
+        while curr.next:
+            if curr.next.val == key:
+                return
+            curr = curr.next
+        curr.next = ListNode(key)
 
     def remove(self, key: int) -> None:
+        hash_val = key % 10_000
+        curr = self.hash_map[hash_val]
+        while curr.next:
+            if curr.next.val == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
 
     def contains(self, key: int) -> bool:
-
+        hash_val = key % 10_000
+        curr = self.hash_map[hash_val]
+        while curr.next:
+            if curr.next.val == key:
+                return True
+            curr = curr.next
+        return False
