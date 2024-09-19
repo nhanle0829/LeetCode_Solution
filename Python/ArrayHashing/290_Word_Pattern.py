1a61108c370024965dc61153ac8fbd98253c1390
Nhan Le <nhanle0829.work@gmail.com>
2024-09-19 09:44:33 -0700

    add solution for #290

--- Python/ArrayHashing/290_Word_Pattern.py
+++ Python/ArrayHashing/290_Word_Pattern.py
@@ -1,3 +1,14 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        
+        s = s.split()
+        if len(pattern) != len(s):
+            return False
+
+        pattern_to_word, word_to_pattern = {}, {}
+        for i in range(len(pattern)):
+            if ((pattern[i] in pattern_to_word and pattern_to_word[pattern[i]] != s[i]) or
+                    (s[i] in word_to_pattern and word_to_pattern[s[i]] != pattern[i])):
+                return False
+            pattern_to_word[pattern[i]] = s[i]
+            word_to_pattern[s[i]] = pattern[i]
+        return True
