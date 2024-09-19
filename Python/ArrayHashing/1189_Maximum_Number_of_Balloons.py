d17988d1d014566277d39060b373b8d50a7a349a
Nhan Le <nhanle0829.work@gmail.com>
2024-09-19 09:34:12 -0700

    add solution for #1189

--- Python/ArrayHashing/1189_Maximum_Number_of_Balloons.py
+++ Python/ArrayHashing/1189_Maximum_Number_of_Balloons.py
@@ -1,3 +1,8 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        
+        text_counter = Counter(text)
+        balloon_counter = Counter("balloon")
+        max_balloon = float("inf")
+        for char in balloon_counter:
+            max_balloon = min(max_balloon, text_counter[char] // balloon_counter[char])
+        return max_balloon
