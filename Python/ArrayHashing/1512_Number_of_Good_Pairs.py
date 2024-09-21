0f2b70975dcb693e5e18131dcc15273707d124a5
Nhan Le <nhanle0829.work@gmail.com>
2024-09-20 21:13:48 -0700

    add solution for #1512

--- Python/ArrayHashing/1512_Number_of_Good_Pairs.py
+++ Python/ArrayHashing/1512_Number_of_Good_Pairs.py
@@ -1,2 +1,7 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        res = 0
+        for count in counter.values():
+            res += count * (count - 1) // 2
+        return res
