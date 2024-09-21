9706e1d717b754989d9cccd5fcaceb4554bdf8a5
Nhan Le <nhanle0829.work@gmail.com>
2024-09-20 21:03:17 -0700

    add solution for #896

--- Python/ArrayHashing/896_Monotonic_Array.py
+++ Python/ArrayHashing/896_Monotonic_Array.py
@@ -1,3 +1,9 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        
+        increased, decreased = True, True
+        for i in range(1, len(nums)):
+            if nums[i - 1] > nums[i]:
+                increased = False
+            if nums[i - 1] < nums[i]:
+                decreased = False
+        return increased or decreased
