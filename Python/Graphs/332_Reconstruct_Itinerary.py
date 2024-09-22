5a572d1b4dc4a589f1aab76727f02eba602652ee
Nhan Le <nhanle0829.work@gmail.com>
2024-09-21 22:31:50 -0700

    add solution for #332

--- Python/Graphs/332_Reconstruct_Itinerary.py
+++ Python/Graphs/332_Reconstruct_Itinerary.py
@@ -1,3 +1,15 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        
+        graph = defaultdict(list)
+        for src, dst in sorted(tickets, reverse=True):
+            graph[src].append(dst)
+
+        res = []
+
+        def dfs(src):
+            while graph[src]:
+                dfs(graph[src].pop())
+            res.append(src)
+
+        dfs("JFK")
+        return res[::-1]
