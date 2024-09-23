b1445322cc768756b7730f6ae38139878658d8a5
Nhan Le <nhanle0829.work@gmail.com>
2024-09-22 20:52:28 -0700

    add solution for #119

--- Python/ArrayHashing/119_Pascal's_Triangle_II.py
+++ Python/ArrayHashing/119_Pascal's_Triangle_II.py
@@ -1,3 +1,10 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        
+        curr_row = [1]
+        for i in range(1, rowIndex + 1):
+            new_row = [1]
+            for j in range(1, i):
+                new_row.append(curr_row[j] + curr_row[j - 1])
+            new_row.append(1)
+            curr_row = new_row
+        return curr_row
