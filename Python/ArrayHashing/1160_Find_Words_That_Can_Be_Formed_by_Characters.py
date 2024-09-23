5f839792fa058b423780b074741b4d51c51a303a
Nhan Le <nhanle0829.work@gmail.com>
2024-09-22 21:37:39 -0700

    add solution for #1160

--- Python/ArrayHashing/1160_Find_Words_That_Can_Be_Formed_by_Characters.py
+++ Python/ArrayHashing/1160_Find_Words_That_Can_Be_Formed_by_Characters.py
@@ -1,3 +1,19 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        
+        char_map = [0] * 26
+        for char in chars:
+            char_map[ord(char) - ord('a')] += 1
+
+        res = 0
+        for word in words:
+            temp = char_map[:]
+            good = True
+            for char in word:
+                index = ord(char) - ord('a')
+                temp[index] -= 1
+                if temp[index] < 0:
+                    good = False
+                    break
+            if good:
+                res += len(word)
+        return res
