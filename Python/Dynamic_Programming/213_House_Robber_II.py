f55774053052ed9f34a0e5229ca3fc755f5ac799
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 12:53:50 -0700

    add solution for #213

--- Python/Dynamic_Programming/213_House_Robber_II.py
+++ Python/Dynamic_Programming/213_House_Robber_II.py
@@ -1,3 +1,15 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        
+    def rob(self, nums: list[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        rob1, rob2 = 0, 0
+        for num in nums[1:]:
+            rob2, rob1 = max(num + rob1, rob2), rob2
+        attempt_1 = rob2
+
+        rob1, rob2 = 0, 0
+        for num in nums[:-1]:
+            rob2, rob1 = max(num + rob1, rob2), rob2
+        attempt_2 = rob2
+        return max(attempt_1, attempt_2)
