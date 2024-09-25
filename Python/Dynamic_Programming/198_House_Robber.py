181849d2295fd218b129a3d7b8869b5842968d0e
Nhan Le <nhanle0829.work@gmail.com>
2024-09-25 12:48:21 -0700

    add solution for #198

--- Python/Dynamic_Programming/198_House_Robber.py
+++ Python/Dynamic_Programming/198_House_Robber.py
@@ -1,3 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
+        rob1, rob2 = 0, 0
+        for num in nums:
+            rob2, rob1 = max(num + rob1, rob2), rob2
+        return rob2
