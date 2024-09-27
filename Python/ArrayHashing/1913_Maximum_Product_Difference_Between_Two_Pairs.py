fe676f7d152e5505b1349134fa92a566abe87bac
Nhan Le <nhanle0829.work@gmail.com>
2024-09-27 12:14:35 -0700

    add solution for #1913

--- Python/ArrayHashing/1913_Maximum_Product_Difference_Between_Two_Pairs.py
+++ Python/ArrayHashing/1913_Maximum_Product_Difference_Between_Two_Pairs.py
@@ -1,3 +1,14 @@
 class Solution:
-    def maxProductDifference(self, nums: List[int]) -> int:
-        
+    def maxProductDifference(self, nums: list[int]) -> int:
+        max_1, max_2 = 0, 0
+        min_1, min_2 = float("inf"), float("inf")
+        for num in nums:
+            if num > max_1:
+                max_1, max_2 = num, max_1
+            elif num > max_2:
+                max_2 = num
+            if num < min_1:
+                min_1, min_2 = num, min_1
+            elif num < min_2:
+                min_2 = num
+        return max_1 * max_2 - min_1 * min_2
