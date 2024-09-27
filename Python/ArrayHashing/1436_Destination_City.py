86ca1a4c45907e55f1e8a63b08d74e34f9c611b8
Nhan Le <nhanle0829.work@gmail.com>
2024-09-27 11:54:05 -0700

    add solution for #1436

--- Python/ArrayHashing/1436_Destination_City.py
+++ Python/ArrayHashing/1436_Destination_City.py
@@ -1,3 +1,9 @@
 class Solution:
-    def destCity(self, paths: List[List[str]]) -> str:
-        
+    def destCity(self, paths: list[list[str]]) -> str:
+        res = set()
+        for src, dst in paths:
+            res.add(dst)
+        for src, dst in paths:
+            if src in res:
+                res.remove(src)
+        return list(res)[0]
