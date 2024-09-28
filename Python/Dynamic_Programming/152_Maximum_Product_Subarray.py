98c81c1a88117b8c8b9f8bf86425594d3d93c5a7
Nhan Le <nhanle0829.work@gmail.com>
2024-09-28 14:42:26 -0700

    add solution for #152

--- Python/Dynamic_Programming/152_Maximum_Product_Subarray.py
+++ Python/Dynamic_Programming/152_Maximum_Product_Subarray.py
@@ -1,3 +1,10 @@
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
-    
+        res = nums[0]
+        curr_min, curr_max = 1, 1
+        for num in nums:
+            temp = curr_max * num
+            curr_max = max(temp, curr_min * num, num)
+            curr_min = min(temp, curr_min * num, num)
+            res = max(res, curr_max)
+        return res
