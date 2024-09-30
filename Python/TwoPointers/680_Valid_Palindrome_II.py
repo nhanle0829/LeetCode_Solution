0515f10ff4a20a33e05b2268d47c4400956d2607
Nhan Le <nhanle0829.work@gmail.com>
2024-09-29 22:51:39 -0700

    add solution for #680

--- Python/TwoPointers/680_Valid_Palindrome_II.py
+++ Python/TwoPointers/680_Valid_Palindrome_II.py
@@ -1,3 +1,17 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return self.checkPalindrome(l + 1, r, s) or self.checkPalindrome(l, r - 1, s)
+            l += 1;
+            r -= 1
+        return True
+
+    def checkPalindrome(self, left, right, s):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1;
+            right -= 1
+        return True
