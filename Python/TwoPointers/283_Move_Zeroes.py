cb446110773a6d690c70543642704383b771b6d7
Nhan Le <nhanle0829.work@gmail.com>
2024-09-30 23:26:01 -0700

    add solution for #283

--- Python/TwoPointers/283_Move_Zeroes.py
+++ Python/TwoPointers/283_Move_Zeroes.py
@@ -1,3 +1,7 @@
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
-        
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
