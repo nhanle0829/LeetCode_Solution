fc19932526021f4282bbc3c8f880ba80a88df7ea
Nhan Le <nhanle0829.work@gmail.com>
2024-10-01 22:23:15 -0700

    add solution for #1496

--- Python/ArrayHashing/1496_Path_Crossing.py
+++ Python/ArrayHashing/1496_Path_Crossing.py
@@ -1,3 +1,13 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        
+        compass = {"N": (0, 1), "S": (0, -1), "E": (1, 0), "W": (-1, 0)}
+        visited = {(0, 0)}
+        curr = [0, 0]
+        for direction in path:
+            x, y = compass[direction]
+            curr[0] += x;
+            curr[1] += y
+            if (curr[0], curr[1]) in visited:
+                return True
+            visited.add((curr[0], curr[1]))
+        return False
