e2197905803fcaba25c8cf2656e6d0d3548dff5d
Nhan Le <nhanle0829.work@gmail.com>
2024-10-01 22:12:41 -0700

    add solution for #6

--- Python/ArrayHashing/6_Zigzag_Conversion.py
+++ Python/ArrayHashing/6_Zigzag_Conversion.py
@@ -1,3 +1,14 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        
+        if numRows == 1:
+            return s
+
+        res = [""] * numRows
+        down = False
+        curr_row = 0
+        for c in s:
+            res[curr_row] += c
+            if curr_row == 0 or curr_row == numRows - 1:
+                down = not down
+            curr_row += 1 if down else -1
+        return ''.join(res)
