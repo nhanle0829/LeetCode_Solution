de972885c339d6c88fcd3dfc13f87ad9aee79a4a
Nhan Le <nhanle0829.work@gmail.com>
2024-10-02 23:10:06 -0700

    add solution for #1897

--- Python/ArrayHashing/1897_Redistribute_Characters_to_Make_All_Strings_Equal.py
+++ Python/ArrayHashing/1897_Redistribute_Characters_to_Make_All_Strings_Equal.py
@@ -1,3 +1,12 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        
+        counter = {}
+        for word in words:
+            for char in word:
+                counter[char] = counter.get(char, 0) + 1
+
+        total = len(words)
+        for char in counter:
+            if counter[char] % total != 0:
+                return False
+        return True
