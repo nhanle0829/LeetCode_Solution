d4a19ce87ad083b8fc869e1083e3048b039a7a1c
Nhan Le <nhanle0829.work@gmail.com>
2024-10-03 12:50:02 -0700

    add solution for #1624

--- Python/1624_Largest_Substring_Between_Two_Equal_Characters.py
+++ Python/1624_Largest_Substring_Between_Two_Equal_Characters.py
@@ -1,3 +1,13 @@
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        
+        hash_map = {}  # char: [start, end]
+        for i, c in enumerate(s):
+            if c not in hash_map:
+                hash_map[c] = [i, i]
+            else:
+                hash_map[c][1] = i
+
+        res = -1
+        for start, end in hash_map.values():
+            res = max(res, end - start - 1)
+        return res
