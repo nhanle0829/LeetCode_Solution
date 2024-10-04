81acff692b7c6b332b9c0ed10726f79e10cd169c
Nhan Le <nhanle0829.work@gmail.com>
2024-10-04 12:08:25 -0700

    add solution for #349

--- Python/ArrayHashing/349_Intersection_of_Two_Arrays.py
+++ Python/ArrayHashing/349_Intersection_of_Two_Arrays.py
@@ -1,3 +1,9 @@
 class Solution:
     def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        
+        res = []
+        seen = set(nums1)
+        for num in nums2:
+            if num in seen:
+                res.append(num)
+                seen.remove(num)
+        return res
