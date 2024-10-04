5feda58384932d55fd321c0c833dd000ee9812ae
Nhan Le <nhanle0829.work@gmail.com>
2024-10-03 19:25:17 -0700

    add solution for #645

--- Python/ArrayHashing/645_Set_Mismatch.py
+++ Python/ArrayHashing/645_Set_Mismatch.py
@@ -1,3 +1,12 @@
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
-        
+        res = [0, 0]  # duplicate, missing
+        hash_set = [0] * len(nums)
+        for num in nums:
+            if hash_set[num - 1] == 1:
+                res[0] = num
+            hash_set[num - 1] += 1
+        for i in range(len(nums)):
+            if hash_set[i] == 0:
+                res[1] = i + 1
+        return res
