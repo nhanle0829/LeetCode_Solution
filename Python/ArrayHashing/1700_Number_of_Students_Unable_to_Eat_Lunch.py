354c8e92fc341ea8236843d844f5aec0f2f8d548
Nhan Le <nhanle0829.work@gmail.com>
2024-10-06 16:20:43 -0700

    add solution for #1700

--- Python/ArrayHashing/1700_Number_of_Students_Unable_to_Eat_Lunch.py
+++ Python/ArrayHashing/1700_Number_of_Students_Unable_to_Eat_Lunch.py
@@ -1,3 +1,11 @@
 class Solution:
     def countStudents(self, students: list[int], sandwiches: list[int]) -> int:
-        
+        counter = [0, 0]
+        for student in students:
+            counter[student] += 1
+
+        for sandwich in sandwiches:
+            if counter[sandwich] == 0:
+                break
+            counter[sandwich] -= 1
+        return sum(counter)
