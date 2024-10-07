6d04500a4e520648b6e993b3024ecc91c81e73a0
Nhan Le <nhanle0829.work@gmail.com>
2024-10-06 17:45:45 -0700

    add solution for #1608

--- Python/ArrayHashing/1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py
+++ Python/ArrayHashing/1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py
@@ -1,3 +1,14 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        
+        length = len(nums)
+        count = [0] * (length + 1)
+        for num in nums:
+            index = min(length, num)
+            count[index] += 1
+
+        total_right = 0
+        for i in range(length, 0, -1):
+            total_right += count[i]
+            if i == total_right:
+                return i
+        return -1
