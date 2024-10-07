a37034c5f0ab76b79dac57e88544b23501202260
Nhan Le <nhanle0829.work@gmail.com>
2024-10-07 14:27:10 -0700

    add solution for #75

--- Python/ArrayHashing/75_Sort_Colors.py
+++ Python/ArrayHashing/75_Sort_Colors.py
@@ -1,3 +1,14 @@
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
-        
+        low, high = 0, len(nums) - 1
+        mid = 0
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1;
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
