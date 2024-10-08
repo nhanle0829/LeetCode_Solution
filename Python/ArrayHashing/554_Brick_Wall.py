33c172818392be73b80d26ff56b0e2ee259ffb0b
Nhan Le <nhanle0829.work@gmail.com>
2024-10-07 23:03:53 -0700

    add solution for #554

--- Python/ArrayHashing/554_Brick_Wall.py
+++ Python/ArrayHashing/554_Brick_Wall.py
@@ -1,2 +1,9 @@
 class Solution:
     def leastBricks(self, wall: list[list[int]]) -> int:
+        gap = {0: 0}
+        for row in wall:
+            position = 0
+            for brick in row[:-1]:
+                position += brick
+                gap[position] = gap.get(position, 0) + 1
+        return len(wall) - max(gap.values())
