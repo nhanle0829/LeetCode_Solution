ae12097c4489b3f9898e804454304212cac9f950
Nhan Le <nhanle0829.work@gmail.com>
2024-10-07 22:29:27 -0700

    add solution for #535

--- Python/ArrayHashing/535_Encode_and_Decode_TinyURL.py
+++ Python/ArrayHashing/535_Encode_and_Decode_TinyURL.py
@@ -1,6 +1,16 @@
 class Codec:
     def __init__(self):
+        self.encoder = {}
+        self.decoder = {}
+        self.url_base = "http://tinyurl.com/"
 
     def encode(self, longUrl: str) -> str:
+        if longUrl not in self.encoder:
+            shortUrl = self.url_base + str(len(self.encoder) + 1)
+            self.encoder[longUrl] = shortUrl
+            self.decoder[shortUrl] = longUrl
+        return self.encoder[longUrl]
 
     def decode(self, shortUrl: str) -> str:
+        return self.decoder[shortUrl]
+
