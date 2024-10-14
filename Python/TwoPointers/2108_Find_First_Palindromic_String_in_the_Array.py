1adf1e80a1640f4110f7cb118e96bfb048fe5a40
Nhan Le <nhanle0829.work@gmail.com>
2024-10-13 21:58:38 -0700

    add solution for #2108

--- Python/TwoPointers/2108_Find_First_Palindromic_String_in_the_Array.py
+++ Python/TwoPointers/2108_Find_First_Palindromic_String_in_the_Array.py
@@ -1,3 +1,14 @@
 class Solution:
-    def firstPalindrome(self, words: List[str]) -> str:
-        
+    def firstPalindrome(self, words: list[str]) -> str:
+        for word in words:
+            if self.isPalidrome(word):
+                return word
+        return ""
+
+    def isPalidrome(self, word):
+        l, r = 0, len(word) - 1
+        while l < r:
+            if word[l] != word[r]:
+                return False
+            l += 1; r -= 1
+        return True
