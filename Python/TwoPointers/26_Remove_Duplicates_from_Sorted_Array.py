a7fe688ac0b1edb88499039e9f80789cbe3797e9
Nhan Le <nhanle0829.work@gmail.com>
2024-10-13 21:41:25 -0700

    add solution for #26

--- Python/TwoPointers/26_Remove_Duplicates_from_Sorted_Array.py
+++ Python/TwoPointers/26_Remove_Duplicates_from_Sorted_Array.py
@@ -1,3 +1,9 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        
+    def removeDuplicates(self, nums: list[int]) -> int:
+        left, right = 0, 1
+        while right < len(nums):
+            if nums[right] != nums[left]:
+                nums[left + 1] = nums[right]
+                left += 1
+            right += 1
+        return left + 1
