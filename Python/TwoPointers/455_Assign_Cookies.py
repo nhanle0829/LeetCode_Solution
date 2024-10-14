c47001bd0b6528ba95e7ae5ec0adfc877c857684
Nhan Le <nhanle0829.work@gmail.com>
2024-10-13 21:53:40 -0700

    add solution for #455

--- Python/TwoPointers/455_Assign_Cookies.py
+++ Python/TwoPointers/455_Assign_Cookies.py
@@ -1,3 +1,12 @@
 class Solution:
     def findContentChildren(self, g: list[int], s: list[int]) -> int:
-        
+        g.sort()
+        s.sort()
+        res = 0
+        i, j = 0, 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                res += 1
+                i += 1
+            j += 1
+        return res
