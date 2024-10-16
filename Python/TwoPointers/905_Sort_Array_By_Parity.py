154871a369382435f092ba5883ec902b90d59648
Nhan Le <nhanle0829.work@gmail.com>
2024-10-16 14:39:49 -0700

    add solution for #905

--- Python/TwoPointers/905_Sort_Array_By_Parity.py
+++ Python/TwoPointers/905_Sort_Array_By_Parity.py
@@ -1,3 +1,8 @@
 class Solution:
     def sortArrayByParity(self, nums: list[int]) -> list[int]:
-        
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] % 2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        return nums
