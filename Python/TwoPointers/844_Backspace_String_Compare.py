0d870cc1309e16ce1c66b0244f73dd3cffbfccf2
Nhan Le <nhanle0829.work@gmail.com>
2025-02-12 21:16:39 -0800

    add solution for #844

--- Python/TwoPointers/844_Backspace_String_Compare.py
+++ Python/TwoPointers/844_Backspace_String_Compare.py
@@ -1,3 +1,13 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        
+        def convert(s):
+            res = []
+            for char in s:
+                if char == '#':
+                    if res:
+                        res.pop()
+                else:
+                    res.append(char)
+            return res
+
+        return convert(s) == convert(t)
