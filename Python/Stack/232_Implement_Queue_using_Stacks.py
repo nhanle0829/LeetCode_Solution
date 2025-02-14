b0d3b492969c8014e234a36644f6ff4239e02ae5
Nhan Le <nhanle0829.work@gmail.com>
2025-02-14 13:22:59 -0800

    add solution for #232

--- Python/Stack/232_Implement_Queue_using_Stacks.py
+++ Python/Stack/232_Implement_Queue_using_Stacks.py
@@ -1,11 +1,18 @@
 class MyQueue:
-
     def __init__(self):
+        self.main = []
+        self.temp = []
 
     def push(self, x: int) -> None:
+        self.temp.append(x)
+        self.temp.extend(self.main)
+        self.main, self.temp = self.temp, []
 
     def pop(self) -> int:
+        return self.main.pop()
 
     def peek(self) -> int:
+        return self.main[-1]
 
     def empty(self) -> bool:
+        return not self.main
