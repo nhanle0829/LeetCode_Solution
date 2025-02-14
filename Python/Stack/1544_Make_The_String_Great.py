de1f50a9bbf664e0ce87091afd64de50e80ddf7b
Nhan Le <nhanle0829.work@gmail.com>
2025-02-14 13:36:28 -0800

    add solution for #1544

--- Python/Stack/1544_Make_The_String_Great.py
+++ Python/Stack/1544_Make_The_String_Great.py
@@ -1,3 +1,9 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        
+        stk = []
+        for c in s:
+            if stk and abs(ord(stk[-1]) - ord(c)) == 32:
+                stk.pop()
+            else:
+                stk.append(c)
+        return ''.join(stk)
