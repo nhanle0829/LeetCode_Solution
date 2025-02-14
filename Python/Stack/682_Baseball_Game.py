deb733357c63300f81e0112779b0147fa74b5730
Nhan Le <nhanle0829.work@gmail.com>
2025-02-13 22:45:18 -0800

    add solution for #682

--- Python/Stack/682_Baseball_Game.py
+++ Python/Stack/682_Baseball_Game.py
@@ -1,2 +1,13 @@
 class Solution:
     def calPoints(self, operations: list[str]) -> int:
+        stk = []
+        for operation in operations:
+            if operation == '+':
+                stk.append(stk[-1] + stk[-2])
+            elif operation == 'D':
+                stk.append(stk[-1] * 2)
+            elif operation == 'C':
+                stk.pop()
+            else:
+                stk.append(int(operation))
+        return sum(stk)
