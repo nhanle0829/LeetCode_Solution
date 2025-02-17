e4596f5bcc458a6d7af87effe7f113458f7f38ca
Nhan Le <nhanle0829.work@gmail.com>
2025-02-17 01:03:51 -0800

    add solution for #83

--- Python/LinkedList/83_Remove_Duplicates_from_Sorted_List.py
+++ Python/LinkedList/83_Remove_Duplicates_from_Sorted_List.py
@@ -6,4 +6,10 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        curr = head
+        while curr and curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
