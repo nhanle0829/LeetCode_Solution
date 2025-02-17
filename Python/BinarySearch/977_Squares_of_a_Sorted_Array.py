d9d51f196e2b2b7f1423a62dca070872e5a58b32
Nhan Le <nhanle0829.work@gmail.com>
2025-02-16 17:15:08 -0800

    add solution for #977

--- Python/BinarySearch/977_Squares_of_a_Sorted_Array.py
+++ Python/BinarySearch/977_Squares_of_a_Sorted_Array.py
@@ -1,3 +1,15 @@
 class Solution:
     def sortedSquares(self, nums: list[int]) -> list[int]:
-        
+        res = [0] * len(nums)
+        l, r = 0, len(nums) - 1
+        res_index = len(nums) - 1
+
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
+                res[res_index] = nums[l] * nums[l]
+                l += 1
+            else:
+                res[res_index] = nums[r] * nums[r]
+                r -= 1
+            res_index -= 1
+        return res
